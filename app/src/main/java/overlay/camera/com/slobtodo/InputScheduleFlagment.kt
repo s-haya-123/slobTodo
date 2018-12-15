7d54eb1cd82590aecf74e4534128d40c03fca650
s-haya-123 <ganbaru.wakenai@gmail.com>
2018-12-15 23:53:51 +0900

    refactoring key listner

--- app/src/main/java/overlay/camera/com/slobtodo/InputScheduleFlagment.kt
+++ app/src/main/java/overlay/camera/com/slobtodo/InputScheduleFlagment.kt
@@ -9,6 +9,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.view.inputmethod.EditorInfo
+import android.widget.EditText
 import android.widget.LinearLayout
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.input_layout.*
@@ -16,32 +17,27 @@ import kotlinx.android.synthetic.main.input_line.*
 import kotlinx.android.synthetic.main.input_line.view.*
 
 class InputScheduleFlagment: Fragment() {
+    private val enterEvent = object:View.OnKeyListener{
+        override fun onKey(v:View,keyCode:Int,event:KeyEvent): Boolean {
+            if(keyCode == KeyEvent.KEYCODE_ENTER){
+                createInpputLine()
+                return true
+            }
+            return false
+        }
+    }
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,savedInstanceState: Bundle?): View? {
         return inflater.inflate(R.layout.input_layout, container, false)
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        input_text.setOnKeyListener(){v,keyCode,event->
-            Log.d("event",keyCode.toString())
-            if(keyCode == KeyEvent.KEYCODE_ENTER){
-                createInpputLine()
-                true
-            }
-            false
-        }
+        input_text.setOnKeyListener(enterEvent)
     }
     private fun createInpputLine(){
         val view:View = layoutInflater.inflate(R.layout.input_line, null)
 
-        view.input_text.setOnKeyListener(){v,keyCode,event->
-            Log.d("event",keyCode.toString())
-            if(keyCode == KeyEvent.KEYCODE_ENTER){
-                createInpputLine()
-                true
-            }
-            false
-        }
+        view.input_text.setOnKeyListener(enterEvent)
         input_list.addView(view)
         return
     }
