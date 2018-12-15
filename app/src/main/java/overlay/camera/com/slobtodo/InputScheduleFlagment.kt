1f0dd5e87757cf46ff51111a6e620efd598749f2
s-haya-123 <ganbaru.wakenai@gmail.com>
2018-12-15 16:48:00 +0900

    add input layout on input activity

--- app/src/main/java/overlay/camera/com/slobtodo/InputScheduleFlagment.kt
+++ app/src/main/java/overlay/camera/com/slobtodo/InputScheduleFlagment.kt
@@ -6,12 +6,24 @@ import android.support.v4.app.FragmentManager
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.LinearLayout
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.input_layout.*
+import kotlinx.android.synthetic.main.input_line.*
 
 class InputScheduleFlagment: Fragment() {
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,savedInstanceState: Bundle?): View? {
         return inflater.inflate(R.layout.input_layout, container, false)
     }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+    private fun createInpputLine(){
+        layoutInflater.inflate(R.layout.input_line, input_list)
+        return
+    }
+
     companion object {
         fun newInstance(): InputScheduleFlagment {
             val fragment = InputScheduleFlagment()
--- app/src/main/java/overlay/camera/com/slobtodo/ScheduleListFlagment.kt
+++ app/src/main/java/overlay/camera/com/slobtodo/ScheduleListFlagment.kt
@@ -5,11 +5,13 @@ import android.support.v4.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.LinearLayout
 
 class ScheduleListFlagment: Fragment() {
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         return inflater.inflate(R.layout.activity_main, container, false)
     }
+
     companion object {
         fun newInstance(): ScheduleListFlagment {
             val fragment = ScheduleListFlagment()
