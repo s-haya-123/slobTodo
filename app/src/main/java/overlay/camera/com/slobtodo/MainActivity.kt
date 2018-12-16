4cfaf7951fb23b4dd881ebac392b2541ea25d614
s-haya-123 <ganbaru.wakenai@gmail.com>
2018-12-16 23:59:20 +0900

    add scroll view

--- app/src/main/java/overlay/camera/com/slobtodo/MainActivity.kt
+++ app/src/main/java/overlay/camera/com/slobtodo/MainActivity.kt
@@ -18,7 +18,6 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.content_main)
-//        setSupportActionBar(toolbar)
 
         if(savedInstanceState == null){
             val fragmentManager:FragmentManager = this.supportFragmentManager
