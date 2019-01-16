c7c745c276a9fb523be28200f11b16bdf0f1d0c8
s-haya-123 <ganbaru.wakenai@gmail.com>
2019-01-16 22:56:20 +0900

    [refactoring] separate sql operation function

--- app/src/main/java/overlay/camera/com/slobtodo/InputScheduleFlagment.kt
+++ app/src/main/java/overlay/camera/com/slobtodo/InputScheduleFlagment.kt
@@ -1,5 +1,6 @@
 package overlay.camera.com.slobtodo
 
+import android.content.Context
 import android.os.Bundle
 import android.support.v4.app.Fragment
 import android.support.v4.app.FragmentManager
@@ -60,27 +61,7 @@ class InputScheduleFlagment: Fragment() {
             }
             if (keyCode == KeyEvent.KEYCODE_BACK && event.getAction() == KeyEvent.ACTION_UP) {
                 context?.let {
-                    val dbService:InputDataDBService = InputDataDBService(it,2)
-                    dbService.open()
-                    if(data.id == null){
-                        data.id  = dbService.insertInputData(data)
-                        data.id?.let {
-                            val idRange = dbService.insertLineDataList(data.lineDataArray,it)
-                            data.lineDataArray.forEachIndexed { index, lineData ->
-                                lineData.id = idRange?.elementAt(index)
-                            }
-                        }
-                    } else {
-                        data.lineDataArray.forEach {
-                            var lineData = it
-                            if(it.id == null){
-                                data.id?.let { dbService.insertLine(lineData,it) }
-                            } else {
-                                dbService.updateLine(lineData)
-                            }
-                        }
-                    }
-                    dbService.close()
+                    operateSQLInputData(it)
                 }
                 true
             }
@@ -92,12 +73,10 @@ class InputScheduleFlagment: Fragment() {
 
             override fun onTextChanged(s:CharSequence,  start:Int, count:Int, after:Int) {
                 lineData.todo = s.toString()
-//                Log.d("textChange",lineData.todo)
             }
 
             override fun afterTextChanged(s: Editable?) {
             }
-
         })
         text.setOnFocusChangeListener{_, hasFocus ->
             if(hasFocus){
@@ -108,6 +87,37 @@ class InputScheduleFlagment: Fragment() {
         }
         this.data.lineDataArray.add(lineData)
     }
+
+    private fun operateSQLInputData(context:Context){
+        val dbService:InputDataDBService = InputDataDBService(context,2)
+        dbService.open()
+        if(data.id == null){
+            insertNewInputData(dbService,data)
+        } else {
+            updateAlreadyExistInputData(dbService,data)
+        }
+        dbService.close()
+    }
+    private fun insertNewInputData(dbService:InputDataDBService,inputData: InputData){
+        dbService.insertInputData(inputData)?.let {
+            inputData.id  = it
+            val idRange = dbService.insertLineDataList(inputData.lineDataArray,it)
+            inputData.lineDataArray.forEachIndexed { index, lineData ->
+                lineData.id = idRange?.elementAt(index)
+            }
+        }
+    }
+    private fun updateAlreadyExistInputData(dbService:InputDataDBService,inputData: InputData){
+        inputData.lineDataArray.forEach {
+            var lineData = it
+            if(it.id == null){
+                inputData.id?.let { dbService.insertLine(lineData,it) }
+            } else {
+                dbService.updateLine(lineData)
+            }
+        }
+    }
+
 
 
 
